aa29c6e1bbf7ee786ed94ec5a956105d634d70c0
artyom kabakov <matroskin7@i.ua>
2020-11-12 21:53:46 +0200

    add packages main and spigot api

---
+++ src/main/java/com/gmf/mc/shop/main/ShopPlugin.java
@@ -0,0 +1,16 @@
+package com.gmf.mc.shop.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShopPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
